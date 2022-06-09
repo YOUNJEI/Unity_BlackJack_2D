f1beb5b677fec63dfee9ab50759454470e5ca5fb
YOUNJEI <kyj951211@gmail.com>
2022-06-09 22:48:41 +0900

    update : Deck 구현 & 주석 한글깨짐 fix

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    public Sprite[] sprites;
+    public Card cardPrefab;
 
+    private Card[] cards;
+    private static string[] symbols = { "Heart", "Dia", "Clover", "Spade" };
+    private const int cardCount = 53;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cards = new Card[cardCount];
+
+        for (int sym = 0; sym < 4; sym++)
+        {
+            for (int i = 1; i <= 13; i++)
+            {
+                cards[(sym * 13) + (i - 1)] = Instantiate<Card>(cardPrefab, transform.position, transform.rotation);
+                cards[(sym * 13) + (i - 1)].Setup(i, symbols[sym]);
+            }
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -5,19 +5,11 @@ using UnityEngine;
 public class SpriteManager : MonoBehaviour
 {
     public Sprite[] sprites;
-    public Card cardPrefab;
 
     public Sprite GetSprite(int idx)
     {
         if (idx < 0 && idx > sprites.Length - 1)
             return (null);
         return (sprites[idx]);
-    }
-
-    private void Start()
-    {
-        
-        Card newCard = Instantiate<Card>(cardPrefab, transform.position, transform.rotation);
-        newCard.SetCardNum(13, "Debug");
     }
 }
