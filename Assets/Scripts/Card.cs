5256a9bfd88fe33d522882be5f4a5ec5b77227b8
YOUNJEI <kyj951211@gmail.com>
2022-06-09 23:04:01 +0900

    modified: 한글 깨짐 fix

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// ??? ??
-// cardNum: ??? ??
-// cardSymbol: ??? ??
+// 카드를 표현
+// cardNum: 카드의 숫자
+// cardSymbol: 카드의 문양
 public class Card : MonoBehaviour
 {
     private int cardNum;
     private string cardSymbol;
-    
+
     public int GetCardNum()
     {
         return (this.cardNum);
@@ -35,8 +35,8 @@ public class Card : MonoBehaviour
         SetCardNum(n);
         SetCardSymbol(symbol);
 
-        // ????? ???? ?????? ???? ???? ??
-        // ? ???? ??? ?? ??
+        // 스프라이트 매니저에 등록되어있는 이미지를 불러오기 위해
+        // 각 문양별로 인덱스 조정 작업
         switch (symbol)
         {
             case "Heart":
@@ -55,20 +55,20 @@ public class Card : MonoBehaviour
                 n += 39;
                 break;
         }
-        // ?? ??? ????????? ???? ?? ??? ??
+        // 자식 객체의 스프라이트렌더러를 변경하여 카드 이미지 표현
         transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite =
             GameObject.Find("SpriteManager").GetComponent<SpriteManager>().GetSprite(n);
     }
 
-    
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
