92311007a1077afb1a4f91fd08b6609c56e8fbea
YOUNJEI <kyj951211@gmail.com>
2022-07-24 19:34:23 +0900

    update : Login BTN 기능

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -6,28 +6,48 @@ using UnityEngine.UI;
 public class LoginManager : MonoBehaviour
 {
     [Header("LoginPanel")]
+    public Button LoginBTN;
+    [Header("LoginPanel")]
     public InputField IdInputField;
     public InputField PasswordInputField;
-    public Button LoginBTN;
+    public Button LoginAccessBTN;
     public Button CreateAccountBTN;
     [Header("CreateAccountPanel")]
     public InputField NewIdInputField;
     public InputField NewPasswordInputField;
+    [Header("LobbyUIButton")]
+    public Button[] UIButtons;
 
     private string LoginUrl;
 
     void Start()
     {
         LoginBTN.onClick.AddListener(() => LoginBTNClicked());
+        LoginAccessBTN.onClick.AddListener(() => LoginAccessBTNClicked());
         CreateAccountBTN.onClick.AddListener(() => CreateAccountBTNClicked());
     }
 
     public void LoginBTNClicked()
     {
-        StartCoroutine(LoginCo());
+        SetActiveUIBTNs(false);
+
+        GameObject LoginPanel = GameObject.Find("Canvas").transform.Find("LoginPanel").gameObject;
+        if (LoginPanel != null)
+        {
+            LoginPanel.SetActive(true);
+        }
+        else
+        {
+            SetActiveUIBTNs(true);
+        }
     }
 
-    IEnumerator LoginCo()
+    public void LoginAccessBTNClicked()
+    {
+        StartCoroutine(LoginAccessCo());
+    }
+
+    IEnumerator LoginAccessCo()
     {
         Debug.Log(IdInputField.text);
         Debug.Log(PasswordInputField.text);
@@ -37,5 +57,13 @@ public class LoginManager : MonoBehaviour
     public void CreateAccountBTNClicked()
     {
 
+    }
+
+    private void SetActiveUIBTNs(bool setBoolean)
+    {
+        for (int i = 0; i < UIButtons.Length; i++)
+        {
+            UIButtons[i].gameObject.SetActive(setBoolean);
+        }
     }
 }
