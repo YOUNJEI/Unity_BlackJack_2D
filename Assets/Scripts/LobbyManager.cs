e4f0cd65a4929816bc4f87e0f3446501d3ed8faa
YOUNJEI <kyj951211@gmail.com>
2022-07-30 16:31:47 +0900

    update : 멀티플레이 접속 UI 패널 작업 중 (1)

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
     // 버튼 프리셋
+    [Header("UI Buttons")]
     public Button BTNSinglePlay;
     public Button BTNMultiPlay;
     public Button BTNExitGame;
+
+    [Header("MultiPlay")]
+    public Text connectinInfoText;
+    public Button BTNRetry;
+    public Button BTNRetryCancel;
+    public Button BTNEnter;
+    private GameObject MultiPlayConnectionPanel;
+
+    private string gameVersion = "1";
 
     private static LobbyManager m_instance;
     public static LobbyManager instance
@@ -28,6 +40,11 @@ public class LobbyManager : MonoBehaviour
         BTNSinglePlay.onClick.AddListener(() => SinglePlayClicked());
         BTNMultiPlay.onClick.AddListener(() => MultiPlayClicked());
         BTNExitGame.onClick.AddListener(() => ExitGameClicked());
+        BTNRetry.onClick.AddListener(() => BTNRetryCancelClicked());
+        BTNRetryCancel.onClick.AddListener(() => BTNRetryCancelClicked());
+        BTNEnter.onClick.AddListener(() => BTNEnterClicked());
+
+        MultiPlayConnectionPanel = GameObject.Find("Canvas").transform.Find("MultiPlayConnectionPanel").gameObject;
     }
 
     private void SinglePlayClicked()
@@ -37,7 +54,55 @@ public class LobbyManager : MonoBehaviour
 
     private void MultiPlayClicked()
     {
+        MultiPlayConnectionPanel.SetActive(true);
+        connectinInfoText.text = "Requeset server connection...";
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
 
+    public override void OnConnectedToMaster()
+    {
+        connectinInfoText.text = "OK";
+        BTNEnter.gameObject.SetActive(true);
+        BTNEnter.interactable = true;
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        BTNRetry.gameObject.SetActive(true);
+        BTNRetryCancel.gameObject.SetActive(true);
+        connectinInfoText.text = "Connection is failed, Retry ?";
+    }
+
+    private void BTNRetryClicked()
+    {
+        connectinInfoText.text = "Requeset server connection...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void BTNRetryCancelClicked()
+    {
+        BTNRetry.gameObject.SetActive(false);
+        BTNRetryCancel.gameObject.SetActive(false);
+        MultiPlayConnectionPanel.SetActive(false);
+    }
+
+    private void BTNEnterClicked()
+    {
+        BTNEnter.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectinInfoText.text = "Enter the room...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            BTNEnter.gameObject.SetActive(false);
+            BTNRetry.gameObject.SetActive(true);
+            BTNRetryCancel.gameObject.SetActive(true);
+            connectinInfoText.text = "Connection is failed, Retry ?";
+        }
     }
 
     private void ExitGameClicked()
